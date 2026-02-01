c0183410d0637b78190a77a9ce44367aba546c57
Isha <ishasovasaria@gmail.com>
2026-02-01 09:12:05 +0800

    Add echo functionality to Bytey

--- src/main/java/Bytey.java
+++ src/main/java/Bytey.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 public class Bytey {
     private static final String LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         showGreeting();
-        printExit();
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println(LINE);
+            if (input.equals("bye")) {
+                printExit();
+                break;
+            }
+            echoInput(input);
+        }
     }
 
     private static void showGreeting() {
         System.out.println(LINE);
         System.out.println(" Hello! I'm Bytey");
         System.out.println(" What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    private static void echoInput(String input) {
+        System.out.println(" " + input);
         System.out.println(LINE);
     }
 
